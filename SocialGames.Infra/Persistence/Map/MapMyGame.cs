d8fb13e7507b06da6efcb1ca996ede13cabba783
juliodive <76788075+juliodive@users.noreply.github.com>
2022-02-03 17:25:28 -0400

    ToDo MapMyGame

---
+++ SocialGames.Infra/Persistence/Map/MapMyGame.cs
@@ -0,0 +1,16 @@
+﻿using SocialGames.Domain.Entities;
+using System.Data.Entity.ModelConfiguration;
+
+namespace SocialGames.Infra.Persistence.Map
+{
+    public class MapMyGame : EntityTypeConfiguration<MyGame>
+    {
+        public MapMyGame()
+        {
+            ToTable("MyGame");
+
+           
+        }
+    
+    }
+}
