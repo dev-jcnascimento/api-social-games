2d6ff1b14f4b4eae790bb065d915ea8435f49082
juliodive <juliodive@gmail.com>
2022-02-18 09:52:26 -0400

    Exception

--- SocialGames.Domain/Entities/Player.cs
+++ SocialGames.Domain/Entities/Player.cs
@@ -13,8 +13,7 @@ namespace SocialGames.Domain.Entities
         public Email Email { get; private set; }
         public Password Password { get; private set; }
         public PlayerStatus Status { get; private set; }
-        public ICollection<PlatForm> PlatForms { get; private set; }
-        public ICollection<Game> Games { get; private set; }
+        public ICollection<MyGame> MyGames { get; private set; }
         protected Player()
         {
         }
