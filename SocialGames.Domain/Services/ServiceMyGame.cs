449ad93ec3caf89440e7a26b4384bbeea9d9d785
juliodive <juliodive@gmail.com>
2022-02-18 18:31:54 -0400

    Refactoring MyGame

--- SocialGames.Domain/Services/ServiceMyGame.cs
+++ SocialGames.Domain/Services/ServiceMyGame.cs
@@ -15,23 +15,19 @@ namespace SocialGames.Domain.Services
         private readonly IRepositoryMyGame _repositoryMyGame;
         private readonly IRepositoryGame _repositoryGame;
         private readonly IRepositoryPlayer _repositoryPlayer;
-        private readonly IServiceGame _serviceGame;
-        private readonly IServicePlayer _servicePlayer;
-        private readonly IServicePlatForm _servicePlatForm;
 
-        public ServiceMyGame(IRepositoryMyGame repositoryMyGame, IRepositoryGame repositoryGame, IRepositoryPlayer repositoryPlayer, IServiceGame serviceGame, IServicePlayer servicePlayer, IServicePlatForm servicePlatForm)
+        public ServiceMyGame(IRepositoryMyGame repositoryMyGame, IRepositoryGame repositoryGame, IRepositoryPlayer repositoryPlayer)
         {
             _repositoryMyGame = repositoryMyGame;
             _repositoryGame = repositoryGame;
             _repositoryPlayer = repositoryPlayer;
-            _serviceGame = serviceGame;
-            _servicePlayer = servicePlayer;
-            _servicePlatForm = servicePlatForm;
         }
 
         public MyGameResponse Create(CreateMyGameRequest request)
         {
-          
+            ExistPlayer(request.PlayerId);
+            ExistGame(request.GameId);
+
             var myGame = new MyGame(request.PlayerId, request.GameId);
 
             if (_repositoryMyGame.Exists(x => x.Player.Id == request.PlayerId && x.Game.Id == request.GameId))
@@ -62,29 +58,22 @@ namespace SocialGames.Domain.Services
         }
         public IEnumerable<MyGameResponse> GetByPlayerId(Guid playerId)
         {
-            _servicePlayer.GetById(playerId);
+            ExistPlayer(playerId);
             var allMyGames = _repositoryMyGame.List(x => x.Game, y => y.Player).Where(x => x.PlayerId == playerId).ToList();
             List<MyGameResponse> listMyGame = new List<MyGameResponse>();
             foreach (var myGame in allMyGames)
             {
-                var platform = _servicePlatForm.GetById(myGame.Game.PlatFormId);
-                listMyGame.Add(new MyGameResponse()
-                {
-                    Id = myGame.Id,
-                    Date = myGame.Date.ToString(),
-                    PlayerName = myGame.Player.Name.ToString(),
-                    GameName = myGame.Game.Name.ToString(),
-                    StatusGame = myGame.MyGameStatus.ToString(),
-                    PlatformName = platform.Name,
-                });
+                var myGameResult = GetByMyGame(myGame.Id);
+                listMyGame.Add(myGameResult);
             }
             return listMyGame;
         }
+
         public MyGameResponse Update(Guid id, UpdateMyGameRequest request)
         {
             var myGame = ExistMyGame(id);
 
-            _serviceGame.GetById(request.GameId);
+            ExistGame(request.GameId);
 
             var status = MyGameStatus.NewGame;
 
@@ -100,9 +89,8 @@ namespace SocialGames.Domain.Services
 
             myGame.Update(status, request.GameId);
             var result = _repositoryMyGame.Update(myGame);
-            result = _repositoryMyGame.List(x => x.Game, y => y.Player).Where(x => x.Id == result.Id).FirstOrDefault();
 
-            return (MyGameResponse)result;
+            return GetByMyGame(result.Id);
         }
 
         public void Delete(Guid id)
@@ -110,20 +98,24 @@ namespace SocialGames.Domain.Services
             var myGame = ExistMyGame(id);
             _repositoryMyGame.Delete(myGame);
         }
+
         private void ExistPlayer(Guid playerId)
         {
             if (_repositoryPlayer.GetById(playerId) == null) throw new ValidationException("Id Player not found!");
         }
+
         private void ExistGame(Guid gameId)
         {
             if (_repositoryGame.GetById(gameId) == null) throw new ValidationException("Id Game not found!");
         }
+
         private MyGame ExistMyGame(Guid id)
         {
             var myGame = _repositoryMyGame.GetById(id);
             if (myGame == null) throw new ValidationException("Id MyGame not found!");
             return myGame;
         }
+
         private MyGameResponse GetByMyGame(Guid myGameId)
         {
             var myGame = ExistMyGame(myGameId);
